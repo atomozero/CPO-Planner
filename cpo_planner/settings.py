8210ae8131060e7dc0c9a3f191679a211ee0642b
atomozero <bernardi.andrea@gmail.com>
2025-03-03 23:15:27 +0100

    creazione  template

--- cpo_planner/settings.py
+++ cpo_planner/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+
     
     # Le nostre app
     'cpo_core.apps.CpoCoreConfig',
